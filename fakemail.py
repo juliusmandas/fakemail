f412041588653bc7f3e0b9d515e3410f4847d5b8
juliusmandas <30669275+juliusmandas@users.noreply.github.com>
2019-04-10 15:47:17 +0200

    Added structure

--- fakemail.py
+++ fakemail.py
@@ -10,24 +10,32 @@ else:
 	HOST = sys.argv[1]
 	FAKEMAIL = sys.argv[2]
 	TARGET = sys.argv[3]
-	MSG = sys.argv[4]
+	MESSAGE = sys.argv[4]
 
+	# SMTP Protocol build
+	HELO = ("HELO " + HOST + "\r\n").encode()
+	FROM = ("MAIL FROM: <" + FAKEMAIL + ">\r\n").encode()
+	TO = ("RCPT TO: <" + TARGET + ">\r\n").encode()
+	DATA = ("DATA\r\nFrom: " + FAKEMAIL[0:FAKEMAIL.find("@")] + " <" + FAKEMAIL + ">\r\nTo: " + TARGET[0:TARGET.find("@")] + " <" + TARGET + ">\r\nSubject: " + TARGET[0:TARGET.find("@")] + "\r\n\r\n").encode()
+	MSG = (MESSAGE + "\r\n.\r\n").encode()
+
+	# Program start
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 	sock.connect((HOST, 25))
-	print(sock.recv(256).decode("UTF-8"))
+	print(sock.recv(256).decode())
 
-	sock.send(("HELO " + HOST + "\r\n").encode("UTF-8"))
-	print(sock.recv(256).decode("UTF-8"))
+	sock.send(HELO)
+	print(sock.recv(256).decode())
 
-	sock.send(("MAIL FROM: <"+ FAKEMAIL +">\r\n").encode("UTF-8"))
-	print(sock.recv(256).decode("UTF-8"))
+	sock.send(FROM)
+	print(sock.recv(256).decode())
 
-	sock.send(("RCPT TO: <"+ TARGET +">\r\n").encode("UTF-8"))
-	print(sock.recv(256).decode("UTF-8"))
+	sock.send(TO)
+	print(sock.recv(256).decode())
 
-	sock.send(("DATA\r\n").encode("UTF-8"))
-	print(sock.recv(256).decode("UTF-8"))
+	sock.send(DATA)
+	print(sock.recv(256).decode())
 
-	sock.send(("From: " + FAKEMAIL[0:FAKEMAIL.find("@")] + " <" + FAKEMAIL + ">\r\nTo: " + TARGET[0:TARGET.find("@")] + " <" + TARGET + ">\r\nSubject: " + TARGET[0:TARGET.find("@")] + "\r\n\r\n" + MSG +"\r\n.\r\n").encode("UTF-8"))
-	print(sock.recv(256).decode("UTF-8"))
+	sock.send(MSG)
+	print(sock.recv(256).decode())
 	sock.close()
